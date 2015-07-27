700a67f54be5ed68f20ec2905dddf654b0563369
yaegaki <yaegaki.fino@gmail.com>
2015-07-28 08:21:28 +0900

    pmtの断片を受け取った気にいくつかのESをロストしてしまう問題を修正

--- example/es.js
+++ example/es.js
@@ -6,7 +6,10 @@ var fs = require('fs');
 var input = fs.createReadStream('input.ts');
 var pat = null;
 var pmtPids = [];
-var pmtMap = {};
+var pmtMap = {
+	complete: {},
+	fragment: {}
+};
 
 function isPAT(tsPacket) {
 	return tsPacket.pid === 0;
@@ -17,8 +20,8 @@ function isPMT(tsPacket) {
 }
 
 function isES(tsPacket) {
-	for (var i in pmtMap) {
-		var pmt = pmtMap[i];
+	for (var i in pmtMap.complete) {
+		var pmt = pmtMap.complete[i];
 		if (!pmt.fragment) {
 			var esPids = pmt.esInfos.map(function (esInfo) { return esInfo.pid; });
 			if (esPids.indexOf(tsPacket.pid) >= 0) {
@@ -37,12 +40,27 @@ input.pipe(new M2TsStream())
 			pat = new M2TsStream.PAT(tsPacket);
 			pmtPids = pat.pmtInfos.map(function (info) { return info.pid; });
 		} else if (isPMT(tsPacket)) {
-			var lastPmt = pmtMap[tsPacket.pid];
-			if (lastPmt !== undefined && lastPmt.fragment) {
-				lastPmt.addFragmentData(tsPacket);
+			var lastPmt = pmtMap.fragment[tsPacket.pid];
+			if (lastPmt != null) {
+				if (lastPmt.addFragmentData(tsPacket)) {
+					if (lastPmt.fragment) {
+						pmtMap.fragment[lastPmt.pid] = lastPmt;
+					} else {
+						pmtMap.complete[lastPmt.pid] = lastPmt;
+						pmtMap.fragment[lastPmt.pid] = null;
+					}
+				} else {
+					pmtMap.fragment[lastPmt.pid] = null;
+				}
 			} else {
 				var pmt = new M2TsStream.PMT(tsPacket);
-				pmtMap[pmt.pid] = pmt;
+				if (!pmt.invalid) {
+					if (pmt.fragment) {
+						pmtMap.fragment[pmt.pid] = pmt;
+					} else {
+						pmtMap.complete[pmt.pid] = pmt;
+					}
+				}
 			}
 		} else if (isES(tsPacket)) {
 			console.log(tsPacket);
